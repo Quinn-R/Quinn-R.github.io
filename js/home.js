8126e141ff965338789c679fd04b22c935b3e478
Quinn Reilly <Quinn.Reilly@hotmail.com>
2018-06-09 15:13:07 +0100

    progress 3

--- js/home.js
+++ js/home.js
@@ -1,6 +1,40 @@
+var menuOpen = 0;
+var menuOpenSize = 100, menuContentOpenSize = 90;
+var menuCloseSize = 10, menuContentCloseSize = 0;
+var result = "";
+
 function menuSize()
 {
-	var menuOpen = 0;
-	var menuOpenSize = 100, menuContentOpenSize = 90;
-	var menuCloseSize = 100, menuContentCloseSize = 0;
+	if(menuOpen == 0)
+	{
+		menuOpen = 1;
+		
+		result = menuOpenSize + "vw";
+		
+		document.getElementById('menu').style.width= result;
+		
+		result = menuContentOpenSize + "vw";
+		
+		document.getElementById('menuContent').style.width = result;
+	}
+	else 
+	{
+		menuOpen = 0;
+		
+		result = menuCloseSize + "vw";
+		
+		document.getElementById('menu').style.width = result;
+		
+		result = menuContentCloseSize + "vw";
+		
+		document.getElementById('menuContent').style.width = result;
+	}
 }
+
+/*
+
+# Copy/paste bin
+
+document.getElementById('div_register').style.height= result;
+
+*/
